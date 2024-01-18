5d3496400c459e91cde9298738aa5ccd953648d2
Meher Vamsi Dontoju <75484855+Mehervamsi1@users.noreply.github.com>
2024-01-18 00:00:39 +0000

    Please enter the commit message for your changes. Lines starting  with '#' will be ignored, and an empty message aborts the commit.
     On branch dev
     Your branch is up to date with 'origin/dev'.
    
     Changes to be committed:#
    	new file:   fintrack/expenses/__pycache__/__init__.cpython-310.pyc
    	new file:   fintrack/expenses/__pycache__/admin.cpython-310.pyc
    	new file:   fintrack/expenses/__pycache__/apps.cpython-310.pyc
    	new file:   fintrack/expenses/__pycache__/models.cpython-310.pyc
    	new file:   fintrack/expenses/__pycache__/urls.cpython-310.pyc#
    	new file:   fintrack/expenses/__pycache__/views.cpython-310.pyc
    	new file:   fintrack/expenses/migrations/__pycache__/__init__.cpython-310.pyc
    	new file:   fintrack/fintrack/__pycache__/__init__.cpython-310.pyc
    	new file:   fintrack/fintrack/__pycache__/settings.cpython-310.pyc
    	new file:   fintrack/fintrack/__pycache__/urls.cpython-310.pyc#
    	new file:   fintrack/fintrack/__pycache__/wsgi.cpython-310.pyc
    	modified:   fintrack/fintrack/settings.py
    	modified:   fintrack/templates/base.html
    	new file:   reuirements.txt
     Untracked files:
    	fintrack/fintrack/static/js/colourmodes.js

--- fintrack/fintrack/settings.py
+++ fintrack/fintrack/settings.py
@@ -77,15 +77,23 @@ WSGI_APPLICATION = 'fintrack.wsgi.application'
 
 DATABASES = {
     
+    
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'fintrackdbnew',
-        'USER': 'postgres',
-        'PASSWORD': 'password',
-        'HOST': 'localhost'
-        #'PORT': '5432'
+        'ENGINE': 'django.db.backends.sqlite3',
         
     } 
+    
+    #This is the connection to the postgresql
+    
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.postgresql',
+    #     'NAME': 'fintrackdbnew',
+    #     'USER': 'postgres',
+    #     'PASSWORD': 'password',
+    #     'HOST': 'localhost'
+    #     #'PORT': '5432'
+        
+    # } 
     
     # 'default': {
     #     'ENGINE': 'django.db.backends.postgresql',
@@ -134,9 +142,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
-SATTICFILES_DIRS = [os.path.join(BASE_DIR),'fintrack/static']
-STATIC_ROOT = os.path.join(BASE_DIR,'static')
+
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'fintrack/static')]
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 
 # Default primary key field type
